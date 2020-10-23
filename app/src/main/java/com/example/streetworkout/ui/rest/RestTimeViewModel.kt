f838713f8b0f8a7b3b4d236232d58b540ea7c2dd
trungtungtang <trungnt62@wru.vn>
2020-10-23 09:38:56 +0700

    Task#30566

---
+++ app/src/main/java/com/example/streetworkout/ui/rest/RestTimeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.streetworkout.ui.rest
+
+import com.example.streetworkout.base.RxViewModel
+
+class RestTimeViewModel : RxViewModel() {
+    fun skipRestTimeTraining() {}
+    fun updateRestTime() {}
+}
